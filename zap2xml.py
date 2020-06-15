afe5ff9795f95b7b43c29ac8fac0e8e9f28747cf
Anthony Lieuallen <arantius@gmail.com>
2020-06-14 20:20:41 -0400

    Gather a full week of (cached) data.

--- zap2xml.py
+++ zap2xml.py
@@ -5,17 +5,17 @@ Around June 2020 the `zap2xml.pl` I had stopped working.  It generated HTTP
 requests that gave only 400 responses.  I tried to patch it, to the point that
 it got OK responses, but parsed no data from them.  The zap2it site must have
 changed.  I thought they had an API, but apparently this tool has always
-scraped the internal JSON feed, built just for the web site?
+scraped the internal JSON feed, intended just for the web site?
 
 So re-write from scratch.  Simplest possible form I can, so the fewest things
 need to change if the site ever does again.  The goal is to feed guide data
 into Tvheadend.
 
-The zap2it site, at least for my area/OTA, only gives 12 hours of data.
-(Without logging in -- and I don't want to write that code!)  So this is
-designed to be run every 3/6 hours or so.  Every URL is cached, so you can go
-more often with little extra cost.
-(TODO: Log in, fetch up to a week?)
+The zap2it site, at least for my area/OTA, will give "400 Bad Request" errors
+*for certain times* of certain days.  Even their own site does this!  This is
+the error that recently started tripping up `zap2xml.pl`.  So this tool simply
+ignores 400 errors, continuing to gather the data available for other time
+windows.
 
 Written to have only standard library dependencies.
 """
@@ -148,8 +148,8 @@ def main():
   out.set('generator-info-name', 'zap2xml.py')
   out.set('generator-info-url', 'github.com/arantius/zap2xml-py')
 
-  # Fetch 12 hours of data, in `zap_timespan` chunks.
-  for i in range(int(12 / args.zap_timespan)):
+  # Fetch data in `zap_timespan` chunks.
+  for i in range(int(7 * 24 / args.zap_timespan)):
     i_time = zap_time + (i * zap_time_window)
     i_dt = datetime.datetime.fromtimestamp(i_time)
     print('Getting data for', i_dt.isoformat())
